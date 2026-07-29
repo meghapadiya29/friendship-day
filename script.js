b424f31c11671021f4b3fc14ab41cc14aee54be0
meghapadiya29 <meghapadiya29@gmail.com>
2026-07-29 16:32:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+
+yesbtn.addEventListener("click", function () { 
+                        alert("yay! Let's begin our friendshipday surprise);
+                              });
+noBtn.addEventListener("mouse over", function () {
+  noBtn.style.position = "absolute";
+Math.random() * 80 + "%";
+  noBtn.style.top = 
+Math.random() * 80 + "%";
+});
