2305b42673d558fdc069947dac6b57fb8de4eb0a
meghapadiya29 <meghapadiya29@gmail.com>
2026-07-29 18:58:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
 
-yesbtn.addEventListener("click", function () { 
-                        alert("yay! Let's begin our friendshipday surprise);
-                              });
-noBtn.addEventListener("mouse over", function () {
+yesBtn.addEventListener("click", function () { 
+                        window.location.href = "gallery.html";});
+noBtn.addEventListener("mouseover", function () {
   noBtn.style.position = "absolute";
+  noBtn.style.left = 
 Math.random() * 80 + "%";
   noBtn.style.top = 
 Math.random() * 80 + "%";
